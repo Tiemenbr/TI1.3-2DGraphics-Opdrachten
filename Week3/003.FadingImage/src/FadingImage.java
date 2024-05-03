048b6041fc3a7edc5052e05ff800056dfefcd8a6
tiemenbr <tiemenbr@hotmail.nl>
2024-05-03 13:34:45 +0200

    ver 3

--- Week3/003.FadingImage/src/FadingImage.java
+++ Week3/003.FadingImage/src/FadingImage.java
@@ -17,9 +17,9 @@ import java.util.Objects;
 public class FadingImage extends Application {
     private ResizableCanvas canvas;
     private BufferedImage[] backgroundImages;
-    private int currentImage;
-    private double timeSinceImageChange;
-    private final double TIMEBETWEENIMAGECHANGE = 3;
+    private ImageState imageState;
+    public static final double TIMEBETWEENIMAGECHANGE = 3;
+    public static final double TRANSITIONTIME = 1;
 
     @Override
     public void start(Stage stage) throws Exception {
@@ -28,6 +28,7 @@ public class FadingImage extends Application {
         canvas = new ResizableCanvas(g -> draw(g), mainPane);
         mainPane.setCenter(canvas);
         FXGraphics2D g2d = new FXGraphics2D(canvas.getGraphicsContext2D());
+        this.imageState = new DisplayState(this,canvas,backgroundImages,0);
         new AnimationTimer() {
             long last = -1;
 
@@ -61,30 +62,19 @@ public class FadingImage extends Application {
         for (int i = 0; i < images.length; i++) {
             backgroundImages[i] = ImageIO.read(images[i]);
         }
-        this.timeSinceImageChange = 0;
-        this.currentImage = 0;
     }
 
     public void draw(FXGraphics2D graphics) {
-        graphics.setTransform(new AffineTransform());
-        graphics.setBackground(Color.WHITE);
-        graphics.clearRect(0, 0, (int) canvas.getWidth(), (int) canvas.getHeight());
-        graphics.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 1.0f));
-        graphics.drawImage(this.backgroundImages[this.currentImage],
-                AffineTransform.getScaleInstance(canvas.getWidth()/this.backgroundImages[this.currentImage].getWidth(),
-                        canvas.getHeight()/this.backgroundImages[this.currentImage].getHeight()),
-                null);
+        this.imageState.draw(graphics);
     }
 
 
     public void update(double deltaTime) {
-        this.timeSinceImageChange += deltaTime;
-        if (this.timeSinceImageChange > this.TIMEBETWEENIMAGECHANGE) {
-            this.currentImage++;
-            if (this.currentImage > this.backgroundImages.length-1)
-                this.currentImage = 0;
-            this.timeSinceImageChange = 0;
-        }
+        this.imageState.update(deltaTime);
+    }
+
+    public void changeState(ImageState state) {
+        this.imageState = state;
     }
 
     public static void main(String[] args) {
---
+++ Week3/003.FadingImage/src/FadingState.java
@@ -0,0 +1,53 @@
+import org.jfree.fx.FXGraphics2D;
+import org.jfree.fx.ResizableCanvas;
+
+import java.awt.*;
+import java.awt.geom.AffineTransform;
+import java.awt.image.BufferedImage;
+
+public class FadingState implements ImageState {
+    private FadingImage fadingImage;
+    private ResizableCanvas canvas;
+    private BufferedImage[] backgroundImages;
+    private int currentImage;
+    private int lastImage;
+    private double timeSinceStateChange;
+
+    public FadingState(FadingImage fadingImage, ResizableCanvas canvas, BufferedImage[] backgroundImages, int currentImage) {
+        this.fadingImage = fadingImage;
+        this.canvas = canvas;
+        this.backgroundImages = backgroundImages;
+        this.currentImage = currentImage;
+        this.lastImage = currentImage - 1;
+        if (this.lastImage < 0)
+            this.lastImage = this.backgroundImages.length - 1;
+        this.timeSinceStateChange = 0;
+    }
+
+    @Override
+    public void draw(FXGraphics2D graphics) {
+        graphics.setTransform(new AffineTransform());
+        graphics.setBackground(Color.WHITE);
+        graphics.clearRect(0, 0, (int) canvas.getWidth(), (int) canvas.getHeight());
+        graphics.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER,
+                (float) (this.timeSinceStateChange == 0 ? 1 : 1 - this.timeSinceStateChange /FadingImage.TRANSITIONTIME)));
+        graphics.drawImage(this.backgroundImages[this.lastImage],
+                AffineTransform.getScaleInstance(canvas.getWidth()/this.backgroundImages[this.lastImage].getWidth(),
+                        canvas.getHeight()/this.backgroundImages[this.lastImage].getHeight()),
+                null);
+
+        graphics.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER,
+                (float) (this.timeSinceStateChange == 0 ? 0 : this.timeSinceStateChange /FadingImage.TRANSITIONTIME)));
+        graphics.drawImage(this.backgroundImages[this.currentImage],
+                AffineTransform.getScaleInstance(canvas.getWidth()/this.backgroundImages[this.currentImage].getWidth(),
+                        canvas.getHeight()/this.backgroundImages[this.currentImage].getHeight()),
+                null);
+    }
+
+    @Override
+    public void update(double deltaTime) {
+        this.timeSinceStateChange += deltaTime;
+        if (this.timeSinceStateChange > FadingImage.TRANSITIONTIME)
+            this.fadingImage.changeState(new DisplayState(this.fadingImage, this.canvas, this.backgroundImages, this.currentImage));
+    }
+}
